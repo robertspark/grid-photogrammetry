06cc9ce364f3af34406a27513302d3344fe70409
Rob Cl <rclandrover@gmail.com>
2025-01-07 13:59:09 +0000

    Update droneModels.js

--- droneModels.js
+++ droneModels.js
@@ -1,395 +1,393 @@
-export default [
-  {
+export default [{
     name: 'DJI Air 3S',
     sensorWidth: 13.1072,
     sensorHeight: 9.8304,
     focalLength: 8.7,
     imageWidth: 8192,
     imageHeight: 6144
-  }, {
+}, {
     name: 'DJI Air 3 (48mp)',
     sensorWidth: 9.690912,
     sensorHeight: 7.27776,
     focalLength: 6.72,
     imageWidth: 8064,
     imageHeight: 6048
-  }, {
+}, {
     name: 'DJI Air 3 (12mp)',
     sensorWidth: 9.690912,
     sensorHeight: 7.27776,
     focalLength: 6.72,
     imageWidth: 4032,
     imageHeight: 3024
-  }, {
+}, {
     name: 'DJI Air 2S',
     sensorWidth: 13.31,
     sensorHeight: 8.88,
     focalLength: 8.4,
     imageWidth: 5472,
     imageHeight: 3648
-  }, {
+}, {
     name: 'DJI Air 2',
     sensorWidth: 6.4,
     sensorHeight: 4.2,
     focalLength: 4.49,
     imageWidth: 8000,
     imageHeight: 6000
-  }, {
+}, {
     name: 'DJI Mavic Air',
     sensorWidth: 6.3,
     sensorHeight: 4.7,
     focalLength: 4.9,
     imageWidth: 4056,
     imageHeight: 3040
-  }, {
+}, {
     name: 'DJI Mini 4 Pro (48mp)',
     sensorWidth: 9.690912,
     sensorHeight: 7.27776,
     focalLength: 6.72,
     imageWidth: 8064,
     imageHeight: 6048
-  }, {
+}, {
     name: 'DJI Mini 4 Pro (12mp)',
     sensorWidth: 9.690912,
     sensorHeight: 7.27776,
     focalLength: 6.72,
     imageWidth: 4032,
     imageHeight: 3024
-  }, {
+}, {
     name: 'DJI Mini 4 Pro (12mp)',
     sensorWidth: 9.690912,
     sensorHeight: 7.27776,
     focalLength: 6.72,
     imageWidth: 4032,
     imageHeight: 3024
-  }, {
+}, {
     name: 'DJI Mini 3 & 3 Pro (48mp)',
     sensorWidth: 9.690912,
     sensorHeight: 7.27776,
     focalLength: 6.72,
     imageWidth: 8064,
     imageHeight: 6048
-  }, {
+}, {
     name: 'DJI Mini 3 & 3 Pro (12mp)',
     sensorWidth: 9.690912,
     sensorHeight: 7.27776,
     focalLength: 6.72,
     imageWidth: 4032,
     imageHeight: 3024
-  }, {
+}, {
     name: 'DJI Mini 4K / 2SE',
     sensorWidth: 6.3,
     sensorHeight: 4.7,
     focalLength: 4.49,
     imageWidth: 4000,
     imageHeight: 3000
-  }, {
+}, {
     name: 'DJI Mini SE',
     sensorWidth: 6.3,
     sensorHeight: 4.7,
     focalLength: 4.49,
     imageWidth: 4000,
     imageHeight: 3000
-  }, {
+}, {
     name: 'DJI Mini 2',
     sensorWidth: 6.3,
     sensorHeight: 4.7,
     focalLength: 4.49,
     imageWidth: 4000,
     imageHeight: 3000
-  }, {
+}, {
     name: 'DJI Mavic Mini',
     sensorWidth: 6.3,
     sensorHeight: 4.7,
     focalLength: 4.49,
     imageWidth: 4000,
     imageHeight: 3000
-  }, {
+}, {
     name: 'DJI Mavic AIR 2',
     sensorWidth: 6.4,
     sensorHeight: 4.8,
     focalLength: 4.49,
     imageWidth: 8000,
     imageHeight: 6000
-  }, {
+}, {
     name: 'DJI Mavic 3 Wide (24mm)',
     sensorWidth: 17.4,
     sensorHeight: 13.1,
     focalLength: 12.29,
     imageWidth: 5280,
     imageHeight: 3956
-  }, {
+}, {
     name: 'DJI Mavic 3 Med(70mm/48mp)',
     sensorWidth: 9.690912,
     sensorHeight: 7.27776,
     focalLength: 19.4,
     imageWidth: 8064,
     imageHeight: 6048
-  }, {
+}, {
     name: 'DJI Mavic 3 Med(70mm/12mp)',
     sensorWidth: 9.690912,
     sensorHeight: 7.27776,
     focalLength: 19.4,
     imageWidth: 4032,
     imageHeight: 3024
-  }, {
+}, {
     name: 'DJI Mavic 3 Tele (166mm)',
     sensorWidth: 6.3,
     sensorHeight: 4.7,
     focalLength: 30.71,
     imageWidth: 4000,
     imageHeight: 3000
-  }, {
+}, {
     name: 'DJI Mavic 2',
     sensorWidth: 13.2,
     sensorHeight: 8.8,
     focalLength: 8.4,
     imageWidth: 5472,
     imageHeight: 3648
-  }, {
+}, {
     name: 'DJI Mavic 2 Zoom',
     sensorWidth: 6.3,
     sensorHeight: 4.7,
     focalLength: 8.6,
     imageWidth: 4000,
     imageHeight: 3000
-  }, {
+}, {
     name: 'DJI Mavic 2 Pro',
     sensorWidth: 13.8,
     sensorHeight: 8.8,
     focalLength: 10.3,
     imageWidth: 5472,
     imageHeight: 3648
-  }, {
+}, {
     name: 'DJI Phantom 3 & 4',
     sensorWidth: 6.3,
     sensorHeight: 4.7,
     focalLength: 3.61,
     imageWidth: 4000,
     imageHeight: 3000
-  }, {
+}, {
     name: 'DJI Phantom 4 Pro / 2.0 / Advanced',
     sensorWidth: 13.2,
     sensorHeight: 8.8,
     focalLength: 8.8,
     imageWidth: 5472,
     imageHeight: 3648
-  }, {
+}, {
     name: 'Nikon aw100',
     focalLength: 5,
     imageHeight: 3456,
     imageWidth: 4608,
     sensorHeight: 4.62,
     sensorWidth: 6.16
-  }, {
+}, {
     name: 'Canon SX230 HS',
     focalLength: 5,
     imageHeight: 3000,
     imageWidth: 4000,
     sensorHeight: 4.62,
     sensorWidth: 6.16
-  }, {
+}, {
     name: ' Samsung s860',
     focalLength: 6.3,
     imageHeight: 2448,
     imageWidth: 3264,
     sensorHeight: 4.29,
     sensorWidth: 5.76
-  }, {
+}, {
     name: 'Canon SX260-SX280',
     focalLength: 4.5,
     imageHeight: 3000,
     imageWidth: 4000,
     sensorHeight: 4.55,
     sensorWidth: 6.17
-  }, {
+}, {
     name: 'Nex5 25mm',
     focalLength: 25,
     imageHeight: 3056,
     imageWidth: 4592,
     sensorHeight: 15.6,
     sensorWidth: 23.4
-  }, {
+}, {
     name: 'Nex5 16mm',
     focalLength: 16,
     imageHeight: 3056,
     imageWidth: 4592,
     sensorHeight: 15.6,
     sensorWidth: 23.4
-  }, {
+}, {
     name: 'Nex7 16mm',
     focalLength: 16,
     imageHeight: 4000,
     imageWidth: 6000,
     sensorHeight: 15.6,
     sensorWidth: 23.5
-  }, {
+}, {
     name: 'Canon S110-S120',
     focalLength: 5.2,
     imageHeight: 3000,
     imageWidth: 4000,
     sensorHeight: 5.7,
     sensorWidth: 7.6
-  }, {
+}, {
     name: 'Canon ELPH 520HS',
     focalLength: 4,
     imageHeight: 2736,
     imageWidth: 3648,
     sensorHeight: 4.55,
     sensorWidth: 6.17
-  }, {
+}, {
     name: 'Canon ELPH 330HS',
     focalLength: 4.3,
     imageHeight: 2736,
     imageWidth: 3648,
     sensorHeight: 4.55,
     sensorWidth: 6.17
-  }, {
+}, {
     name: 'Canon ELPH 115-130 IS',
     focalLength: 5,
     imageHeight: 3072,
     imageWidth: 4608,
     sensorHeight: 4.55,
     sensorWidth: 6.17
-  }, {
+}, {
     name: 'Canon A2300-A2400',
     focalLength: 5,
     imageHeight: 3456,
     imageWidth: 4608,
     sensorHeight: 4.55,
     sensorWidth: 6.17
-  }, {
+}, {
     name: 'Canon 5D Mk2',
     focalLength: 5,
     imageHeight: 3744,
     imageWidth: 5616,
     sensorHeight: 24,
     sensorWidth: 36
-  }, {
+}, {
     name: 'NX1000',
     focalLength: 20,
     imageHeight: 3648,
     imageWidth: 5472,
     sensorHeight: 15.7,
     sensorWidth: 23.5
-  }, {
+}, {
     name: 'FIREFLY 6S',
     focalLength: 3,
     imageHeight: 3000,
     imageWidth: 4000,
     sensorHeight: 4.7058,
     sensorWidth: 6.2372
-  }, {
+}, {
     name: 'Sony A6000',
     focalLength: 20,
     imageHeight: 4000,
     imageWidth: 6000,
     sensorHeight: 15.6,
     sensorWidth: 23.5
-  }, {
+}, {
     name: 'Sony A7R',
     focalLength: 28,
     imageHeight: 4912,
     imageWidth: 7360,
     sensorHeight: 24.0,
     sensorWidth: 35.9
-  }, {
+}, {
     name: 'Parrot Sequoia Monochrome',
     focalLength: 3.98,
     imageHeight: 960,
     imageWidth: 1280,
     sensorHeight: 3.6,
     sensorWidth: 4.8
-  }, {
+}, {
     name: 'Parrot Sequoia RGB',
     focalLength: 4.88,
     imageHeight: 3456,
     imageWidth: 4608,
     sensorHeight: 4.63,
     sensorWidth: 6.17
-  }, {
+}, {
     name: 'Canon S120',
     focalLength: 5.2,
     imageHeight: 3000,
     imageWidth: 4000,
     sensorHeight: 5.7,
     sensorWidth: 7.6
-  }, {
+}, {
     name: 'WX 500',
     focalLength: 4.1,
     imageHeight: 3264,
     imageWidth: 4896,
     sensorHeight: 4.55,
     sensorWidth: 6.17
-  }, {
+}, {
     name: 'RedEdge',
     focalLength: 5.5,
     imageHeight: 960,
     imageWidth: 1280,
     sensorHeight: 3.6,
     sensorWidth: 4.8
-  }, {
+}, {
     name: 'FLIR Vue 336 6,8mm',
     focalLength: 6.8,
     imageHeight: 256,
     imageWidth: 336,
     sensorHeight: 4.352,
     sensorWidth: 5.712
-  }, {
+}, {
     name: 'FLIR Vue 336 9mm',
     focalLength: 9,
     imageHeight: 256,
     imageWidth: 336,
     sensorHeight: 4.352,
     sensorWidth: 5.712
-  }, {
+}, {
     name: 'FLIR Vue 336 13mm',
     focalLength: 13,
     imageHeight: 256,
     imageWidth: 336,
     sensorHeight: 4.352,
     sensorWidth: 5.712
-  }, {
+}, {
     name: 'FLIR Vue 640 9mm',
     focalLength: 9,
     imageHeight: 512,
     imageWidth: 640,
     sensorHeight: 8.704,
     sensorWidth: 10.88
-  }, {
+}, {
     name: 'FLIR Vue 640 13mm',
     focalLength: 13,
     imageHeight: 512,
     imageWidth: 640,
     sensorHeight: 8.704,
     sensorWidth: 10.88
-  }, {
+}, {
     name: 'FLIR Vue 640 19mm',
     focalLength: 19,
     imageHeight: 512,
     imageWidth: 640,
     sensorHeight: 8.704,
     sensorWidth: 10.88
-  }, {
+}, {
     name: 'GoPro Hero 4 Black Wide',
     focalLength: 2.46,
     imageHeight: 3000,
     imageWidth: 4000,
     sensorHeight: 4.56,
     sensorWidth: 6.17
-  }, {
+}, {
     name: 'Sony a7R2',
     focalLength: 5,
     imageHeight: 3456,
     imageWidth: 4608,
     sensorHeight: 4.62,
     sensorWidth: 6.16
-  }, {
+}, {
     name: 'AeroHawk_8MP',
     focalLength: 3.039,
     imageHeight: 2464,
     imageWidth: 3280,
     sensorHeight: 2.76,
     sensorWidth: 3.674
-  }
-]
+}]
